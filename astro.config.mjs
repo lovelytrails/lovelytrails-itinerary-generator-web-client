cd68599a053975cb704d966e716a4391d9eab50b
Eric Stanley <eric.stanleyjones@gmail.com>
2025-10-14 01:11:23 +0530

    initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+  routes: {
+    404: "/404",
+  }
+});
